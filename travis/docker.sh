84407a33d294034b1861b91e297e1c704432bbc5
gas1121 <jtdzhx@gmail.com>
2017-09-22 10:56:18 +0800

    fix travis build issue

---
+++ travis/docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+# Build docker image
+sudo docker build --rm=true --file docker/cloudmanager/Dockerfile --tag=gas1121/cloud-manager:cloudmanager-test .
+
+# start target service for testing
+sudo docker-compose -f travis/docker-compose.test.yml up -d
+
+# waiting 10 secs
+sleep 10
+
+# run tests
+sudo docker-compose -f travis/docker-compose.test.yml exec cloud-manager nosetests -v --with-coverage --cover-erase
+# get coverage data from container
+sudo docker cp $(sudo docker-compose -f travis/docker-compose.test.yml ps -q cloud-manager):/app/.coverage .
+# send coverage report
+sudo chown travis:travis .coverage
+coveralls
+
+# spin down compose
+sudo docker-compose -f travis/docker-compose.test.yml down
+
+# remove 'test' images
+sudo docker rmi gas1121/cloud-manager:cloudmanager-test
